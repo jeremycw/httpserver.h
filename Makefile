9d3b00c4b5d28a26cf7fa888f70e5d08df7e9ada
Jeremy Williams <jeremy.williams@paytm.com>
2019-12-13 14:49:42 -0500

    Set up basic test suite

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 all: httpserver.h
 
 test: http-server
+	./http-server & test/run > results.txt; kill %1; diff results.txt test/results.txt
 
 http-server: test/main.c httpserver.h
 	$(CC) -O3 test/main.c -o http-server
--- test/main.c
+++ test/main.c
@@ -13,13 +13,16 @@ void handle_request(struct http_request_s* request) {
   http_request_connection(request, HTTP_AUTOMATIC);
   struct http_response_s* response = http_response_init();
   http_response_status(response, 200);
-  http_response_header(response, "Content-Type", "text/plain");
   if (request_target_is(request, "/echo")) {
     http_string_t body = http_request_body(request);
+    http_response_header(response, "Content-Type", "text/plain");
     http_response_body(response, body.buf, body.len);
-  } else if (request_target_is(request, "/user-agent")) {
-    http_string_t ua = http_request_header(request, "User-Agent");
+  } else if (request_target_is(request, "/host")) {
+    http_string_t ua = http_request_header(request, "Host");
+    http_response_header(response, "Content-Type", "text/plain");
     http_response_body(response, ua.buf, ua.len);
+  } else if (request_target_is(request, "/empty")) {
+    // No Body
   } else if (request_target_is(request, "/headers")) {
     int iter = 0, i = 0;
     http_string_t key, val;
@@ -27,8 +30,10 @@ void handle_request(struct http_request_s* request) {
     while (http_request_iterate_headers(request, &key, &val, &iter)) {
       i += snprintf(buf + i, 512 - i, "%.*s: %.*s\n", key.len, key.buf, val.len, val.buf);
     }
+    http_response_header(response, "Content-Type", "text/plain");
     http_response_body(response, buf, i);
   } else {
+    http_response_header(response, "Content-Type", "text/plain");
     http_response_body(response, RESPONSE, sizeof(RESPONSE) - 1);
   }
   http_respond(request, response);
