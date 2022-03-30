a63bdd38ee8e24bf82b159b4b83fe17af4760378
Jeremy Williams <jeremycw2@gmail.com>
2022-03-30 16:19:34 -0400

    Fix formatting of test_parser.c

--- test/unit/test_parser.c
+++ test/unit/test_parser.c
@@ -1,10 +1,11 @@
-#include <unistd.h>
 #include "munit.h"
+#include <unistd.h>
 
 #include "../../src/common.h"
 #include "../../src/parser.h"
 
-void setup_buffer_and_parser(struct hsh_parser_s* parser, struct hsh_buffer_s* buffer, char const* req_str) {
+void setup_buffer_and_parser(struct hsh_parser_s *parser,
+                             struct hsh_buffer_s *buffer, char const *req_str) {
   // setup parser
   hsh_parser_init(parser);
 
@@ -17,43 +18,38 @@ void setup_buffer_and_parser(struct hsh_parser_s* parser, struct hsh_buffer_s* b
   buffer->sequence_id = 1;
 }
 
-MunitResult test_parser_small_body_complete(const MunitParameter params[], void* data) {
+MunitResult test_parser_small_body_complete(const MunitParameter params[],
+                                            void *data) {
   (void)params;
   (void)data;
-  char const* request_string = "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: 16\r\n\r\naaaaaaaaaaaaaaaa";
+  char const *request_string =
+      "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: "
+      "16\r\n\r\naaaaaaaaaaaaaaaa";
 
-  struct hsh_parser_s parser = { 0 };
-  struct hsh_buffer_s buffer = { 0 };
+  struct hsh_parser_s parser = {0};
+  struct hsh_buffer_s buffer = {0};
   setup_buffer_and_parser(&parser, &buffer, request_string);
 
   enum hsh_token_e expected_types[] = {
-    HSH_TOK_METHOD,
-    HSH_TOK_TARGET,
-    HSH_TOK_VERSION,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADERS_DONE,
-    HSH_TOK_BODY
-  };
+      HSH_TOK_METHOD,       HSH_TOK_TARGET,       HSH_TOK_VERSION,
+      HSH_TOK_HEADER_KEY,   HSH_TOK_HEADER_VALUE, HSH_TOK_HEADER_KEY,
+      HSH_TOK_HEADER_VALUE, HSH_TOK_HEADERS_DONE, HSH_TOK_BODY};
 
-  char const* expected_values[] = {
-    "GET",
-    "/foo",
-    "HTTP/1.1",
-    "Host",
-    "www.jeremycw.com",
-    "Content-Length",
-    "16",
-    "",
-    "aaaaaaaaaaaaaaaa"
-  };
+  char const *expected_values[] = {"GET",
+                                   "/foo",
+                                   "HTTP/1.1",
+                                   "Host",
+                                   "www.jeremycw.com",
+                                   "Content-Length",
+                                   "16",
+                                   "",
+                                   "aaaaaaaaaaaaaaaa"};
 
   int i = 0;
   struct hsh_token_s out;
   while ((out = hsh_parser_exec(&parser, &buffer, 1024)).type != HSH_TOK_NONE) {
-    munit_assert_memory_equal(out.len, expected_values[i], &buffer.buf[out.index]);
+    munit_assert_memory_equal(out.len, expected_values[i],
+                              &buffer.buf[out.index]);
     munit_assert(expected_types[i] == out.type);
     if (out.type == HSH_TOK_HEADERS_DONE) {
       munit_assert(out.flags == 0);
@@ -68,41 +64,32 @@ MunitResult test_parser_small_body_complete(const MunitParameter params[], void*
   return MUNIT_OK;
 }
 
-MunitResult test_parser_small_body_partial(const MunitParameter params[], void* data) {
+MunitResult test_parser_small_body_partial(const MunitParameter params[],
+                                           void *data) {
   (void)params;
   (void)data;
-  char const* request_string = "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: 16\r\n\r\naaaaaaaa";
+  char const *request_string =
+      "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: "
+      "16\r\n\r\naaaaaaaa";
 
-  struct hsh_parser_s parser = { 0 };
-  struct hsh_buffer_s buffer = { 0 };
+  struct hsh_parser_s parser = {0};
+  struct hsh_buffer_s buffer = {0};
   setup_buffer_and_parser(&parser, &buffer, request_string);
 
   enum hsh_token_e expected_types[] = {
-    HSH_TOK_METHOD,
-    HSH_TOK_TARGET,
-    HSH_TOK_VERSION,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADERS_DONE
-  };
+      HSH_TOK_METHOD,       HSH_TOK_TARGET,       HSH_TOK_VERSION,
+      HSH_TOK_HEADER_KEY,   HSH_TOK_HEADER_VALUE, HSH_TOK_HEADER_KEY,
+      HSH_TOK_HEADER_VALUE, HSH_TOK_HEADERS_DONE};
 
-  char const* expected_values[] = {
-    "GET",
-    "/foo",
-    "HTTP/1.1",
-    "Host",
-    "www.jeremycw.com",
-    "Content-Length",
-    "16",
-    ""
-  };
+  char const *expected_values[] = {
+      "GET", "/foo", "HTTP/1.1", "Host", "www.jeremycw.com", "Content-Length",
+      "16",  ""};
 
   int i = 0;
   struct hsh_token_s out;
   while ((out = hsh_parser_exec(&parser, &buffer, 1024)).type != HSH_TOK_NONE) {
-    munit_assert_memory_equal(out.len, expected_values[i], &buffer.buf[out.index]);
+    munit_assert_memory_equal(out.len, expected_values[i],
+                              &buffer.buf[out.index]);
     munit_assert(expected_types[i] == out.type);
     i++;
   }
@@ -116,53 +103,42 @@ MunitResult test_parser_small_body_partial(const MunitParameter params[], void* 
   out = hsh_parser_exec(&parser, &buffer, 1024);
 
   munit_assert(out.type == HSH_TOK_BODY);
-  munit_assert_memory_equal(out.len, "aaaaaaaaaaaaaaaa", &buffer.buf[out.index]);
+  munit_assert_memory_equal(out.len, "aaaaaaaaaaaaaaaa",
+                            &buffer.buf[out.index]);
 
   free(buffer.buf);
 
   return MUNIT_OK;
 }
 
-MunitResult test_parser_large_body(const MunitParameter params[], void* data) {
+MunitResult test_parser_large_body(const MunitParameter params[], void *data) {
   (void)params;
   (void)data;
-  char const* request_string = "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: 16\r\n\r\naaaaaaaa";
+  char const *request_string =
+      "GET /foo HTTP/1.1\r\nHost: www.jeremycw.com\r\nContent-Length: "
+      "16\r\n\r\naaaaaaaa";
 
-  struct hsh_parser_s parser = { 0 };
-  struct hsh_buffer_s buffer = { 0 };
+  struct hsh_parser_s parser = {0};
+  struct hsh_buffer_s buffer = {0};
   setup_buffer_and_parser(&parser, &buffer, request_string);
 
-
   enum hsh_token_e expected_types[] = {
-    HSH_TOK_METHOD,
-    HSH_TOK_TARGET,
-    HSH_TOK_VERSION,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADERS_DONE,
-    HSH_TOK_BODY
-  };
+      HSH_TOK_METHOD,       HSH_TOK_TARGET,       HSH_TOK_VERSION,
+      HSH_TOK_HEADER_KEY,   HSH_TOK_HEADER_VALUE, HSH_TOK_HEADER_KEY,
+      HSH_TOK_HEADER_VALUE, HSH_TOK_HEADERS_DONE, HSH_TOK_BODY};
 
-  char const* expected_values[] = {
-    "GET",
-    "/foo",
-    "HTTP/1.1",
-    "Host",
-    "www.jeremycw.com",
-    "Content-Length",
-    "16",
-    "",
-    "aaaaaaaa"
-  };
+  char const *expected_values[] = {
+      "GET", "/foo", "HTTP/1.1", "Host", "www.jeremycw.com", "Content-Length",
+      "16",  "",     "aaaaaaaa"};
 
   int i = 0;
   struct hsh_token_s out;
   int max_buf_capacity = strlen(request_string);
 
-  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type != HSH_TOK_NONE) {
-    munit_assert_memory_equal(out.len, expected_values[i], &buffer.buf[out.index]);
+  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type !=
+         HSH_TOK_NONE) {
+    munit_assert_memory_equal(out.len, expected_values[i],
+                              &buffer.buf[out.index]);
     munit_assert(expected_types[i] == out.type);
     i++;
   }
@@ -182,48 +158,44 @@ MunitResult test_parser_large_body(const MunitParameter params[], void* data) {
   return MUNIT_OK;
 }
 
-MunitResult test_parser_chunked_body(const MunitParameter params[], void* data) {
+MunitResult test_parser_chunked_body(const MunitParameter params[],
+                                     void *data) {
   (void)params;
   (void)data;
-  char const* request_string = "POST /chunked/test HTTP/1.0\r\nHost: www.jeremycw.com\r\nTransfer-Encoding: chunked\r\n\r\n5\r\nabcde\r\na\r\n1234567890\r\n0\r\n\r\n";
+  char const *request_string =
+      "POST /chunked/test HTTP/1.0\r\nHost: "
+      "www.jeremycw.com\r\nTransfer-Encoding: "
+      "chunked\r\n\r\n5\r\nabcde\r\na\r\n1234567890\r\n0\r\n\r\n";
 
-  struct hsh_parser_s parser = { 0 };
-  struct hsh_buffer_s buffer = { 0 };
+  struct hsh_parser_s parser = {0};
+  struct hsh_buffer_s buffer = {0};
   setup_buffer_and_parser(&parser, &buffer, request_string);
 
   enum hsh_token_e expected_types[] = {
-    HSH_TOK_METHOD,
-    HSH_TOK_TARGET,
-    HSH_TOK_VERSION,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADERS_DONE,
-    HSH_TOK_BODY,
-    HSH_TOK_BODY,
-    HSH_TOK_BODY
-  };
+      HSH_TOK_METHOD,       HSH_TOK_TARGET,       HSH_TOK_VERSION,
+      HSH_TOK_HEADER_KEY,   HSH_TOK_HEADER_VALUE, HSH_TOK_HEADER_KEY,
+      HSH_TOK_HEADER_VALUE, HSH_TOK_HEADERS_DONE, HSH_TOK_BODY,
+      HSH_TOK_BODY,         HSH_TOK_BODY};
 
-  char const* expected_values[] = {
-    "POST",
-    "/chunked/test",
-    "HTTP/1.0",
-    "Host",
-    "www.jeremycw.com",
-    "Transfer-Encoding",
-    "chunked",
-    "",
-    "abcde",
-    "1234567890",
-    ""
-  };
+  char const *expected_values[] = {"POST",
+                                   "/chunked/test",
+                                   "HTTP/1.0",
+                                   "Host",
+                                   "www.jeremycw.com",
+                                   "Transfer-Encoding",
+                                   "chunked",
+                                   "",
+                                   "abcde",
+                                   "1234567890",
+                                   ""};
 
   int i = 0;
   struct hsh_token_s out;
   int max_buf_capacity = strlen(request_string);
-  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type != HSH_TOK_NONE) {
-    munit_assert_memory_equal(out.len, expected_values[i], &buffer.buf[out.index]);
+  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type !=
+         HSH_TOK_NONE) {
+    munit_assert_memory_equal(out.len, expected_values[i],
+                              &buffer.buf[out.index]);
     munit_assert(expected_types[i] == out.type);
     i++;
   }
@@ -235,44 +207,40 @@ MunitResult test_parser_chunked_body(const MunitParameter params[], void* data) 
   return MUNIT_OK;
 }
 
-MunitResult test_parser_chunked_body_partial(const MunitParameter params[], void* data) {
+MunitResult test_parser_chunked_body_partial(const MunitParameter params[],
+                                             void *data) {
   (void)params;
   (void)data;
-  char const* request_string = "POST /chunked/test HTTP/1.0\r\nHost: www.jeremycw.com\r\nTransfer-Encoding: chunked\r\n\r\n5\r\nabcde\r\na\r\n12345678";
+  char const *request_string = "POST /chunked/test HTTP/1.0\r\nHost: "
+                               "www.jeremycw.com\r\nTransfer-Encoding: "
+                               "chunked\r\n\r\n5\r\nabcde\r\na\r\n12345678";
 
-  struct hsh_parser_s parser = { 0 };
-  struct hsh_buffer_s buffer = { 0 };
+  struct hsh_parser_s parser = {0};
+  struct hsh_buffer_s buffer = {0};
   setup_buffer_and_parser(&parser, &buffer, request_string);
 
   enum hsh_token_e expected_types[] = {
-    HSH_TOK_METHOD,
-    HSH_TOK_TARGET,
-    HSH_TOK_VERSION,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADER_KEY,
-    HSH_TOK_HEADER_VALUE,
-    HSH_TOK_HEADERS_DONE,
-    HSH_TOK_BODY
-  };
+      HSH_TOK_METHOD,       HSH_TOK_TARGET,       HSH_TOK_VERSION,
+      HSH_TOK_HEADER_KEY,   HSH_TOK_HEADER_VALUE, HSH_TOK_HEADER_KEY,
+      HSH_TOK_HEADER_VALUE, HSH_TOK_HEADERS_DONE, HSH_TOK_BODY};
 
-  char const* expected_values[] = {
-    "POST",
-    "/chunked/test",
-    "HTTP/1.0",
-    "Host",
-    "www.jeremycw.com",
-    "Transfer-Encoding",
-    "chunked",
-    "",
-    "abcde"
-  };
+  char const *expected_values[] = {"POST",
+                                   "/chunked/test",
+                                   "HTTP/1.0",
+                                   "Host",
+                                   "www.jeremycw.com",
+                                   "Transfer-Encoding",
+                                   "chunked",
+                                   "",
+                                   "abcde"};
 
   int i = 0;
   struct hsh_token_s out;
   int max_buf_capacity = strlen(request_string);
-  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type != HSH_TOK_NONE) {
-    munit_assert_memory_equal(out.len, expected_values[i], &buffer.buf[out.index]);
+  while ((out = hsh_parser_exec(&parser, &buffer, max_buf_capacity)).type !=
+         HSH_TOK_NONE) {
+    munit_assert_memory_equal(out.len, expected_values[i],
+                              &buffer.buf[out.index]);
     munit_assert(expected_types[i] == out.type);
     i++;
   }
@@ -292,4 +260,3 @@ MunitResult test_parser_chunked_body_partial(const MunitParameter params[], void
 
   return MUNIT_OK;
 }
-
